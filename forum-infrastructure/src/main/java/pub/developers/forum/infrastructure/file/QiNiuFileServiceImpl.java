66b2816e81f4514424496ecdac5ecf90400a8833
wjploop <wjploop@163.com>
2022-01-18 22:06:09 +0800

    配置好七牛云

--- forum-infrastructure/src/main/java/pub/developers/forum/infrastructure/file/QiNiuFileServiceImpl.java
+++ forum-infrastructure/src/main/java/pub/developers/forum/infrastructure/file/QiNiuFileServiceImpl.java
@@ -2,6 +2,7 @@ package pub.developers.forum.infrastructure.file;
 
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import lombok.Data;
@@ -34,7 +35,8 @@ public class QiNiuFileServiceImpl implements FileService {
 
     @Override
     public String uploadImg(byte[] base64, String key) {
-        UploadManager uploadManager = new UploadManager();
+        Configuration configuration = new Configuration();
+        UploadManager uploadManager = new UploadManager(configuration);
         try {
             String token = Auth.create(accessKey, secretKey).uploadToken(bucketName, key);
             CheckUtil.isEmpty(token, ErrorCodeEn.FILE_UPLOAD_FAIL);
@@ -42,7 +44,7 @@ public class QiNiuFileServiceImpl implements FileService {
             Response res = uploadManager.put(base64, key, token);
             CheckUtil.isFalse(res.isOK(), ErrorCodeEn.FILE_UPLOAD_FAIL);
 
-            return accessDomain + res.jsonToObject(Ret.class).key;
+            return accessDomain + "/" + res.jsonToObject(Ret.class).key;
         } catch (QiniuException e) {
             LogUtil.info(log, e, "上传文件异常，e.msg={}", e.getMessage());
             CheckUtil.isTrue(true, ErrorCodeEn.FILE_UPLOAD_FAIL);
